44201030916265fa5739a01a7c6cd75dea149cac
sangala632 <sangala2003@gmail.com>
2026-05-10 08:17:32 +0530

    IAM

---
+++ 40-IAM/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.24.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "terraform-dev-state-remote"
+    key          = "remote-state-iam"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
