e8fef0b78cc54adef12a932b6cf99f82c818587b
sangala632 <sangala2003@gmail.com>
2026-05-10 08:18:35 +0530

    IAM

--- 40-IAM/main.tf
+++ 40-IAM/main.tf
@@ -2,4 +2,4 @@ resource "aws_iam_policy" "alb" {
   name        = "AWSLoadBalancerControllerIAMPolicy"
   description = "ALB Controller permissions"
   policy      = file("${path.module}/iam-policy.json")
-}
+}   
--- 40-IAM/variables.tf
+++ 40-IAM/variables.tf
@@ -6,10 +6,3 @@ variable "environment" {
     default = "dev"
 }
 
-variable "zone_id" {
-    default = "Z0168679G0Y1V181BPMR"
-}
-
-variable "zone_name" {
-    default = "hellodevsecops.space"
-}
